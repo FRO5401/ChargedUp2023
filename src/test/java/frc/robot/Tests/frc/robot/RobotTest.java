afb43d52c7599767447d96a7be97b2996254ec77
Brolsen2232 <info@team5401.org>
2023-06-28 19:06:58 -0400

    Updated build.gradle to include testLogging & ignoreFailures

--- src/test/java/frc/robot/Tests/frc/robot/RobotTest.java
+++ src/test/java/frc/robot/Tests/frc/robot/RobotTest.java
@@ -1,18 +1,30 @@
-package frc.robot;
+package frc.robot.Tests.frc.robot;
 import frc.robot.Subsystems.DriveBase;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class RobotTestTest {
+public class RobotTest {
 
         
     @Test
-    public void test() {
+    public void mathBrokeTest() {
+       assertEquals(1, 2);
+
+    }
+
+    @Test
+    public void mathWorkTest() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void robotTest(){
         DriveBase driveBase = new DriveBase();
         assertEquals(driveBase.getAdjustedSpeed(0, 0), 0.01);
         assertEquals(driveBase.getAdjustedSpeed(0.5, 0.5), 0.01);
         assertEquals(driveBase.getAdjustedSpeed(-0.5, -0.5), 0.01);
         assertEquals(driveBase.getAdjustedSpeed(0.5, -0.5), 0.01);
         assertEquals(driveBase.getAdjustedSpeed(-0.5, 0.5), 0.01);
-
     }
 }
     
